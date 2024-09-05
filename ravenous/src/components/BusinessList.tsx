05614ec8153b1a63717bfd4072d31fa35b4bf1bd
jpcardozx <jpcardozoo0106@gmail.com>
2024-09-04 23:04:06 -0300

    assets update

--- ravenous/src/components/BusinessList.tsx
+++ ravenous/src/components/BusinessList.tsx
@@ -3,7 +3,7 @@ import Business from './Business';
 
 const businesses = [
   {
-    imageSrc: '/api/placeholder/400/300',
+    imageSrc: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     name: "MarginOtto Pizzeria",
     address: "1010 Paddington Way",
     city: "Bordertown",
@@ -14,7 +14,7 @@ const businesses = [
     reviewCount: 90
   },
   {
-    imageSrc: '/api/placeholder/400/300',
+    imageSrc: 'https://images.unsplash.com/photo-1530554764233-e79e16c91d08?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     name: "Burger Queen",
     address: "505 Patty Avenue",
     city: "Burgerville",
@@ -25,7 +25,7 @@ const businesses = [
     reviewCount: 120
   },
   {
-    imageSrc: '/api/placeholder/400/300',
+    imageSrc: 'https://images.unsplash.com/photo-1611143669185-af224c5e3252?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     name: "Sushi Paradise",
     address: "789 Wasabi Lane",
     city: "Rolltown",
